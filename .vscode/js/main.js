3081d1711a4764d6d1ddb8475002a8817559856e
stellalee1210 <yeobinlee1210@gmail.com>
2025-05-24 22:07:54 +0900

    create main page frame

---
+++ .vscode/js/main.js
@@ -0,0 +1,25 @@
+const mainPageTitle = document.getElementById("mainPageTitle");
+const searchBtn = document.getElementById("recipeSearchBtn");
+const loginBtn = document.getElementById("loginBtn");
+const signUpBtn = document.getElementById("signUpBtn");
+
+function onClickMyPageTitle() {
+  console.log("click title");
+}
+function onClickLogin() {
+  //   window.location.href = "login.html";
+  console.log("click log in");
+}
+
+function onClickSignUp() {
+  console.log("click sign up");
+}
+
+function onClickSearchRecipe() {
+  console.log("click search recipe");
+}
+
+mainPageTitle.addEventListener("click", onClickMyPageTitle);
+searchBtn.addEventListener("click", onClickSearchRecipe);
+loginBtn.addEventListener("click", onClickLogin);
+signUpBtn.addEventListener("click", onClickSignUp);
