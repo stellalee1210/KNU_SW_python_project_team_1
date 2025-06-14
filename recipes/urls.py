9e665d1edc854c3fa85318b5cd365a2ed90754a8
Stella <133227322+stellalee1210@users.noreply.github.com>
2025-06-14 19:49:43 +0900

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('search/', views.search_recipes, name='search_recipes'),
+    path('api/search/', views.search_recipes, name='search_recipes'),
 ]
